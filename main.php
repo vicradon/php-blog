33e6716f878be14827183a255cc49738f08590b0
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2021-02-15 06:21:25 +0100

    chore: add structure to post card
    chore: Add author username to post card

--- main.php
+++ main.php
@@ -1,9 +1,15 @@
 <?php
 session_start();
 include('./config/db_connect.php');
-$page = $_GET['page'] ?? 0;
-$sql = "SELECT * FROM `posts` LIMIT {$page}, 10";
+$page_input = $_GET['page'] ?? 0;
+$page = $page_input < 1 ? 1 : $page_input;
+$offset = ($page * 10) - 10;
+$sql = "SELECT posts.id, posts.title, posts.content, users.username FROM `posts` INNER JOIN `users` ON users.id = posts.user_id LIMIT {$offset}, 10";
+$sql_count = "SELECT COUNT(*) as count FROM posts";
+
 $result = $conn->query($sql);
+$posts_count_result = $conn->query($sql_count);
+$posts_count = $posts_count_result->fetch_assoc();
 
 if ($result !== TRUE) {
     $_SESSION['error_flash'] = "An error occured while fetching the posts";
@@ -13,28 +19,63 @@ $posts = $result->fetch_all();
 
 ?>
 
+<style>
+    .post-card {
+        width: 300px;
+    }
+</style>
+
+
 <div class="mt-4">
-    <h1 class="text-center">Users</h1>
+    <h1 class="text-center">All posts</h1>
     <div class="row justify-content-center mt-3">
         <?php if ($result->num_rows > 0) { ?>
             <div class="row justify-content-center">
                 <?php foreach ($posts as $post) : ?>
-                    <a href="/posts/post.php?id=<?php echo htmlspecialchars($post[0]) ?>">
-                        <div class="col mb-5">
-                            <div class="card w-100">
-                                <div class="card-header">
-                                    <h3 class="card-title"> <?php echo htmlspecialchars($post[1]) ?> </h3>
-                                </div>
-                                <div class="card-body">
-                                    <p class="card-text"> <?php echo htmlspecialchars(substr($post[2], 0, 30)) ?>... </p>
-                                </div>
+                    <div class="col mb-5">
+                        <div class="card post-card">
+                            <div class="card-header">
+                                <h3 class="card-title"> <?php echo htmlspecialchars($post[1]) ?> </h3>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text"> <?php echo htmlspecialchars(substr($post[2], 0, 100)) ?>... </p>
+                                <p>Author: <b><?php echo  htmlspecialchars($post[3]) ?></p></b>
+                            </div>
+                            <div class="card-footer">
+                                <a class="btn btn-primary" href="/posts/post.php?id=<?php echo htmlspecialchars($post[0]) ?>">Read</a>
                             </div>
                         </div>
-                    </a>
+                    </div>
                 <?php endforeach ?>
             </div>
         <?php } else { ?>
             <h4>No posts</h4>
         <?php } ?>
+    </div>
+    <div>
+        <ul class="pagination">
+            <li class="page-item <?php echo $page <= 1 ? 'disabled' : 'enabled' ?>">
+                <?php if ($page > 1) { ?>
+                    <a href="?page=<?php echo $page ?>"><span class="page-link">Previous</span></a>
+                <?php } else { ?>
+                    <span class="page-link">Previous</span>
+                <?php }  ?>
+
+            </li>
+            <?php if ($page > 1) { ?>
+                <li class="page-item"><a class="page-link" href="#"><?php echo $page - 1 ?></a></li>
+            <?php } ?>
+
+            <li class="page-item active" aria-current="page">
+                <span class="page-link">
+                    <?php echo $page ?>
+                    <span class="sr-only">(current)</span>
+                </span>
+            </li>
+            <li class="page-item"><a class="page-link" href="#"><?php echo $page + 1 ?></a></li>
+            <li class="page-item">
+                <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
     </div>
 </div>
--- posts/post.php
+++ posts/post.php
@@ -2,7 +2,8 @@
 session_start();
 include('../config/db_connect.php');
 $post_id = $_GET['id'];
-$sql = "select title, content from posts where id='{$post_id}'";
+$sql = "SELECT posts.title, posts.content, users.username FROM `posts` INNER JOIN `users` ON users.id = posts.user_id WHERE posts.id='{$post_id}'";
+
 $result = $conn->query($sql);
 $post = $result->fetch_assoc();
 
@@ -13,6 +14,7 @@ $post = $result->fetch_assoc();
     <?php if ($post) { ?>
         <h1><?php echo $post['title'] ?></h1>
         <p><?php echo $post['content'] ?></p>
+        <p>Author <b><?php echo $post['username'] ?></p></b>
     <?php } else { ?>
         <p><?php echo "No such post exists" ?></p>
     <?php } ?>
